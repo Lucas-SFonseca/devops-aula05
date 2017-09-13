fa6c7ca131c89546bf9e97eb20ddd70722ba6353
luck97 <30813671+luck97@users.noreply.github.com>
2017-09-12 21:58:45 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,1 +1,14 @@
-gfw easrpomjge y
+def inicializar():
+	tab = [ ]
+	for i in range():
+		linha = [ ]
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
+
+def main( ):
+	jogo = inicializar( )
+	print (jogo)
+if __name__ == "__main__":
+	main()
